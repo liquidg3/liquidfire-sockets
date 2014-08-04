28cbc8a06508302ef2b7d655f61a58c2bf0fe5f4
Tay <me@taylorrome.ro>
2014-08-04 16:26:30 -0600

    version bump

--- public/js/Socket.io.js
+++ public/js/Socket.io.js
@@ -241,8 +241,8 @@
                             console.log('error event', data);
                         }
 
-
                         callback(this.coerceEvent(event, data));
+
                     }.bind(this));
                 }
 
